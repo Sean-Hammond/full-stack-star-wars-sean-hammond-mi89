3c43a783c81812416e2f849253fd1058a369c2cb
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-03-20 15:29:14 +0000

    Create some routes

--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorite_characters: Mapped[List["Favorite_Character"]] = relationship(
+        back_populates="user")
+    favorite_planets: Mapped[List["Favorite_Planet"]
+                             ] = relationship(back_populates="user")
+    # favorite_species: Mapped[List["Favorite_Species"]
+    #                          ] = relationship(back_populates="user")
 
     def serialize(self):
         return {
@@ -17,3 +25,100 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    mass: Mapped[int] = mapped_column(nullable=False)
+    height: Mapped[int] = mapped_column(nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(
+        String(500), unique=True, nullable=False)
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    users_who_favorite: Mapped[List["Favorite_Character"]] = relationship(
+        back_populates="character")
+    planet: Mapped["Planet"] = relationship(back_populates="characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass,
+            "height": self.height,
+            "age": self.age,
+            "description": self.description,
+            "planet": self.planet.serialize()
+            # do not serialize the password, its a security breach
+        }
+
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    mass: Mapped[int] = mapped_column(nullable=False)
+    circumference: Mapped[int] = mapped_column(nullable=False)
+    characters: Mapped[List["Character"]] = relationship(
+        back_populates="planet")
+    description: Mapped[str] = mapped_column(
+        String(500), unique=True, nullable=False)
+    users_who_favorite: Mapped[List["Favorite_Planet"]
+                               ] = relationship(back_populates="planet")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mass": self.mass,
+            "circumference": self.circumference,
+            "description": self.description,
+            # do not serialize the password, its a security breach
+        }
+
+
+# class Species(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+#     description: Mapped[str] = mapped_column(
+#         String(500), unique=True, nullable=False)
+#     characters: Mapped[List["Character"]] = relationship(
+#         back_populates="author")
+#     planets: Mapped[List["Planet"]] = relationship(back_populates="author")
+#     users_who_favorite: Mapped[List["Favorite_Species"]
+#                                ] = relationship(back_populates="species")
+
+
+class Favorite_Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"))
+    user: Mapped["User"] = relationship(back_populates="favorite_characters")
+    character: Mapped["Character"] = relationship(
+        back_populates="users_who_favorite")
+
+
+class Favorite_Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    user: Mapped["User"] = relationship(back_populates="favorite_planets")
+    planet: Mapped["Planet"] = relationship(
+        back_populates="users_who_favorite")
+
+
+# class Favorite_Species(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+#     species_id: Mapped[int] = mapped_column(ForeignKey("species.id"))
+#     user: Mapped["User"] = relationship(back_populates="favorite_species")
+#     species: Mapped["Species"] = relationship(
+#         back_populates="users_who_favorite")
+    
+# Terminal code to run if the pipenv run migrate gives a Flask error:
+    # rm -R -f ./migrations &&
+    # pipenv run init &&
+    # dropdb -h localhost -U gitpod example || true &&
+    # createdb -h localhost -U gitpod example || true &&
+    # psql -h localhost example -U gitpod -c 'CREATE EXTENSION unaccent;' || true &&
+    # pipenv run migrate &&
+    # pipenv run upgrade
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet, Favorite_Character, Favorite_Planet
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,51 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/people", methods=["GET"])
+def get_all_people():
+    all_people = db.session.execute(select(Character)).scalars().all()
+    people_dictionaries = []
+    for person in all_people:
+        people_dictionaries.append(person.serialize())
+    return jsonify(people_dictionaries), 200
+
+@api.route("/people/<int:people_id>", methods=["GET"])
+def get_single_person(people_id):
+    single_person = db.session.get(Character, people_id)  # Recommended direct method
+    if single_person is None:
+        return jsonify({"message": "character not found"}), 404
+    return jsonify(single_person.serialize()), 200
+
+@api.route("/planets", methods=["GET"])
+def get_planets():
+    pass
+
+@api.route("/planets/<int:planet_id>", methods=["GET"])
+def get_single_planet(planet_id):
+    pass
+
+@api.route("/user", methods=["GET"])
+def get_users():
+    pass
+
+@api.route("/users/<int:user_id>/favorites", methods=["GET"])
+def get_favorites(user_id):
+    pass
+
+# We will send the user in the body 
+@api.route("/favorite/planet/<int:planet_id>", methods=["POST"])
+def add_favorite_planet(planet_id):
+    pass
+
+@api.route("/favorite/people/<int:people_id>", methods=["POST"])
+def add_favorite_person(people_id):
+    pass
+
+@api.route("/favorite/planet/<int:planet_id>", methods=["DELETE"])
+def delete_favorite_planet(planet_id):
+    pass
+
+@api.route("/favorite/people/<int:people_id>", methods=["DELETE"])
+def delete_favorite_person(people_id):
+    pass
